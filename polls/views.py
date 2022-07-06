a946ae99f95131397c8ebb642191e1f320930a01
vibhor <vats.vibhor.07@gmail.com>
2022-07-06 10:46:37 +0530

    changing the index view 1

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is it, I made it.</h1>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
